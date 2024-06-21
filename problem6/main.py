69e7195def00b94e851e4c1ba3489d888b2d10cf
Kharisma Novi C <168548312+rismanovic24@users.noreply.github.com>
2024-06-21 22:33:01 +0700

    update and commit

--- problem6/main.py
+++ problem6/main.py
@@ -1,7 +1,37 @@
-def full_prima(N):
+def prime_number(N):
     # your code here
-    return ''
+    if N <= 1:
+        return False
+    for i in range (2, N):
+        if N % i == 0:
+            return False
+    return True
 
+def full_prima(N):
+    if N <= 0:
+        return False
+
+    while N > 0:
+        digit = N % 10
+        if not prime_number(digit):
+            return False
+        N //= 10
+    return True
+
+def full_prima(N):
+    str_number = str(N)
+
+    if N <= 0:
+        return False
+
+    for i in range(len(str_number)):
+        if not prime_number(int(str_number[i])):
+            return False
+
+    if not prime_number(N):
+        return False
+
+    return True
 if __name__ == '__main__':
     print(full_prima(2)) # True
     print(full_prima(3)) # True
