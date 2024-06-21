89192f9c5f792384ec93ea0355f0244a8db53fae
Kharisma Novi C <168548312+rismanovic24@users.noreply.github.com>
2024-06-21 22:28:41 +0700

    update and commit

--- problem1/main.py
+++ problem1/main.py
@@ -1,7 +1,20 @@
 # input
 student_score = 80
 
-# Process: Your Solution Code Here
+#output
+num_input = input("Enter score =  ")
+num = int(num_input)
+grade = " "
 
+if num >= 80 :
+    grade = "A"
+elif num >= 65 :
+    grade = "B+"
+elif num >= 50 :
+    grade = "B"
+elif num >= 35 :
+    grade = "C"
+else :
+    grade = "D"
 
-# Output "Nilai A"
+print("with the num" , num , "will give grade" , grade)
