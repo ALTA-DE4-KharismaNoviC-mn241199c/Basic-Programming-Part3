70c24ddbf421fc5e00e9583ae2c4ac129180ae74
Kharisma Novi C <168548312+rismanovic24@users.noreply.github.com>
2024-06-21 22:29:28 +0700

    update and commit

--- problem2.2/main.py
+++ problem2.2/main.py
@@ -1,1 +1,10 @@
-bilangan = int(input())
+bilangan = int(input("masukkan angka =  "))
+
+faktorial = [ ]
+for i in range (1 , bilangan + 1):
+        if bilangan % i == 0:
+            faktorial.append(i)
+
+print("faktorial dari" , bilangan , "sebagai berikut" )
+for i in range(len(faktorial)-1,-1,-1):
+        print(faktorial[i])
