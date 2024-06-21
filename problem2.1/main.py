9df780eb0ef149324ebb17e9a7d4de3e22843121
Kharisma Novi C <168548312+rismanovic24@users.noreply.github.com>
2024-06-21 22:29:04 +0700

    update and commit

--- problem2.1/main.py
+++ problem2.1/main.py
@@ -1,1 +1,8 @@
-bilangan = int(input())
+bilangan = int(input("masukkan angka =  "))
+
+def faktorial(bilangan):
+    for i in range (1 , (bilangan + 1)):
+        if bilangan % i == 0:
+            print (i)
+
+print(faktorial(bilangan))
